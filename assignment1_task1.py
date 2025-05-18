42730dca354509ee46a0e970fc9d1fe6dab0cfe2
kalyan-kiran <kalyankiran.p@gmail.com>
2025-05-18 17:30:05 +0530

    Add files via upload

---
+++ assignment1_task1.py
@@ -0,0 +1,14 @@
+#Task - 1 :  Basic Mathematical operations :
+
+x=input('enter first number : ')
+y=input('enter second number : ')
+x=float(x)
+y=float(y)
+a=x+y  # addition
+b=x-y  # substraction
+c=x*y  # multiplication
+d=x/y  # division
+print('\n sum of given two numbers is : \t',a)
+print('\n difference of given two numbers is \t: ',b)
+print('\n product of given numbers is : \t',c)
+print('\n division of given two numbers is \t: ',d)
---
+++ assignment1_task2.py
@@ -0,0 +1,8 @@
+
+# Task - 2  :  personalized  greeting
+
+fn=input('Enter your First Name: ')
+ln=input('Enter your Last Name: ')
+n=fn+' '+ln
+print('Hello, ',n,'! Welcome to the Python program by Tutedude')
+
